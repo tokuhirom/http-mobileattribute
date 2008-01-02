262169b6cd91c308b4dd8273b187d8092e867f6e
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-01-02 16:54:58 +0000

    HTTP-MobileAttribute: start project
    git-svn-id: http://svn.coderepos.org/share/lang/perl/HTTP-MobileAttribute/trunk@3998 d0d07461-0603-4401-acd4-de1884942a52

---
+++ lib/HTTP/MobileAttribute.pm
@@ -0,0 +1,39 @@
+package HTTP::MobileAttribute;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+use Class::Component;
+
+1;
+__END__
+
+=for stopwords aaaatttt gmail dotottto commmmm Kazuhiro Osawa
+
+=head1 NAME
+
+HTTP::MobileAttribute -
+
+=head1 SYNOPSIS
+
+  use HTTP::MobileAttribute;
+
+=head1 DESCRIPTION
+
+HTTP::MobileAttribute is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom aaaatttt gmail dotottto commmmmE<gt>
+
+Kazuhiro Osawa
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
