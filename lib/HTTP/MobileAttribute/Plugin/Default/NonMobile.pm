29975705f73e8a998d148d8a9f3c35dd290d72ee
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-04-02 10:03:47 +0000

    bug fixed.
    git-svn-id: http://svn.coderepos.org/share/lang/perl/HTTP-MobileAttribute/trunk@8663 d0d07461-0603-4401-acd4-de1884942a52

--- lib/HTTP/MobileAttribute/Plugin/Default/NonMobile.pm
+++ lib/HTTP/MobileAttribute/Plugin/Default/NonMobile.pm
@@ -4,7 +4,7 @@ use warnings;
 use base qw/Class::Component::Plugin/;
 
 sub model           :MobileMethod('model,NonMobile') { '' }
-sub device_id       :MobileMethod('model,NonMobile') { '' }
+sub device_id       :MobileMethod('device_id,NonMobile') { '' }
 sub xhtml_compliant :MobileMethod('xhtml_compliant,NonMobile') { 1 }
 
 1;
