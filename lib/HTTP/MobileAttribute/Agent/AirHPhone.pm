5cf94df15249c7e1f9f598b4cfba23858b6e77b8
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-04-02 03:58:06 +0000

    つかってない
    git-svn-id: http://svn.coderepos.org/share/lang/perl/HTTP-MobileAttribute/trunk@8636 d0d07461-0603-4401-acd4-de1884942a52

--- lib/HTTP/MobileAttribute/Agent/AirHPhone.pm
+++
@@ -1,7 +0,0 @@
-package HTTP::MobileAttribute::Agent::AirHPhone;
-use strict;
-use warnings;
-use base qw/HTTP::MobileAttribute::Agent::Base/;
-
-1;
-
--- lib/HTTP/MobileAttribute/Agent/ThirdForce.pm
+++
@@ -1,7 +0,0 @@
-package HTTP::MobileAttribute::Agent::ThirdForce;
-use strict;
-use warnings;
-use base qw/HTTP::MobileAttribute::Agent::Base/;
-
-1;
-
