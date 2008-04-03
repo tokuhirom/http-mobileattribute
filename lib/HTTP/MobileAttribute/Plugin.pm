ebacf956dcf2acd8a73a0d4a26c9b0089e08daba
yappo <yappo@d0d07461-0603-4401-acd4-de1884942a52>
2008-04-03 07:16:40 +0000

    lang/perl/HTTP-MobileAttribute: Plugin のアクセサメソッドは一度登録したら二度と登録しなくていいよ
    git-svn-id: http://svn.coderepos.org/share/lang/perl/HTTP-MobileAttribute/trunk@8743 d0d07461-0603-4401-acd4-de1884942a52

--- lib/HTTP/MobileAttribute/Plugin.pm
+++ lib/HTTP/MobileAttribute/Plugin.pm
@@ -5,21 +5,23 @@ use base qw/Class::Component::Plugin/;
 
 sub mk_register_accessors {
     my $self    = shift;
+    return if $self->{__method_registerd};
     my $carrier = shift;
     my $class   = ref $self;
 
     my $pkg = HTTP::MobileAttribute->agent_class($carrier);
     for my $method (@_) {
         no strict 'refs';
-        *{"$class\::$method"} = sub { shift->{$method} } unless *{"$class\::$method"}{CODE};
+        *{"$class\::$method"} = sub { shift->{$method} };
         $pkg->register_method( $method => $self );
     }
+    $self->{__method_registerd} = 1;
 }
 
 sub instance_clear : Hook('instance_clear') {
     my($self, $c) = @_;
     for my $key (keys %{ $self }) {
-        delete $self->{$key} unless $key eq 'config';
+        delete $self->{$key} unless $key eq 'config' || $key eq '__method_registerd';
     }
 }
 
