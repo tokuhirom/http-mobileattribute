27873f3ed19af8875e6984b29ef854f17b91dc3c
daisuke <daisuke@d0d07461-0603-4401-acd4-de1884942a52>
2008-04-03 04:48:46 +0000

    lang/perl/HTTP-MobileAttribute; remove closures from Plugin::Display
    git-svn-id: http://svn.coderepos.org/share/lang/perl/HTTP-MobileAttribute/trunk@8726 d0d07461-0603-4401-acd4-de1884942a52

--- lib/HTTP/MobileAttribute/Plugin/Display.pm
+++ lib/HTTP/MobileAttribute/Plugin/Display.pm
@@ -5,47 +5,63 @@ use base qw/HTTP::MobileAttribute::Plugin/;
 use HTTP::MobileAttribute;
 use HTTP::MobileAttribute::Plugin::Display::DoCoMoMap qw/$DisplayMap/;
 
+my %ARG_DISPATCH = (
+    ThirdForce => \&__thirdforce_args,
+    EZWeb      => \&__ezweb_args,
+    DoCoMo     => \&__docomo_args,
+);
+
 sub display : Method {
     my ($self, $c) = @_;
 
-    my $code = +{
-        ThirdForce => sub {
-            my($width, $height) = split /\*/, $c->request->get('x-jphone-display');
-
-            my($color, $depth);
-            if (my $c_str = $c->request->get('x-jphone-color')) {
-                ($color, $depth) = $c_str =~ /^([CG])(\d+)$/;
-            }
-
-            +{
-                width  => $width,
-                height => $height,
-                color  => $color eq 'C',
-                depth  => $depth,
-            };
-        },
-        EZweb => sub {
-            my ( $width, $height ) = split /,/, $c->request->get('x-up-devcap-screenpixels');
-            my $depth = ( split /,/, $c->request->get('x-up-devcap-screendepth') )[0];
-            my $color = $c->request->get('x-up-devcap-iscolor');
-
-            +{
-                width  => $width,
-                height => $height,
-                color  => ( defined $color && $color eq '1' ),
-                depth  => 2**$depth,
-            };
-        },
-        DoCoMo => sub {
-            $DisplayMap->{ uc( $c->model ) };
-        },
-    }->{$c->carrier_longname};
-
+    my $code = $ARG_DISPATCH{$c->carrier_longname};
     if ($code) {
-        HTTP::MobileAttribute::Plugin::Display::Display->new( $code->() );
+        HTTP::MobileAttribute::Plugin::Display::Display->new( $code->($c) );
     } else {
         return;
     }
+}
+
+sub __thirdforce_args
+{
+    my $c = shift;
+    my $request = $c->request;
+
+    my($width, $height) = split /\*/, $request->get('x-jphone-display');
+
+    my($color, $depth);
+    if (my $c_str = $request->get('x-jphone-color')) {
+        ($color, $depth) = $c_str =~ /^([CG])(\d+)$/;
+    }
+
+    +{
+        width  => $width,
+        height => $height,
+        color  => $color eq 'C',
+        depth  => $depth,
+    };
+}
+
+sub __ezweb_args
+{
+    my $c = shift;
+    my $request = $c->request;
+
+    my ( $width, $height ) = split /,/, $request->get('x-up-devcap-screenpixels');
+    my $depth = ( split /,/, $request->get('x-up-devcap-screendepth') )[0];
+    my $color = $request->get('x-up-devcap-iscolor');
+
+    +{
+        width  => $width,
+        height => $height,
+        color  => ( defined $color && $color eq '1' ),
+        depth  => 2**$depth,
+    };
+
+sub __docomo_args
+{
+    my $c = shift;
+    $DisplayMap->{ uc( $c->model ) };
 }
 
 package HTTP::MobileAttribute::Plugin::Display::Display;
