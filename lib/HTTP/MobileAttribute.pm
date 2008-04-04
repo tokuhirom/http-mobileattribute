6b4b73ef17423bb660daa2f0efa92acfe3e8b13d
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-04-04 06:04:26 +0000

    lang/perl/HTTP-MobileAttribute: Checking in changes prior to tagging of version 0.03.  Changelog diff is:
    === Changes
    ==================================================================
    --- Changes	(revision 10274)
    +++ Changes	(local)
    @@ -1,5 +1,18 @@
     Revision history for Perl extension HTTP::MobileAttribute
     
    +0.03 alpha.
    +
    +    - performance tuning(by lestrat&yappo&tokuhirom).
    +    - added more test codes.
    +    - remove GPS from default plugins
    +    - document updated.
    +    - remove html_version.
    +    - renamed Default::* => Parser::*
    +    - Agent::*->xhtml_compliant moved to Plugin::XHTMLComplient.
    +    - remove Default::DoCoMo->cache_size, Default::DoCoMo->vendor, Default::DoCoMo->series
    +    - remove Default::EZweb->is_(win|tuka)
    +    - CarrierMethod is now simple.
    +
     0.02
     
             - Performance Tuning(68x faster)
    Unknown target: CHANGES.
    
    
    git-svn-id: http://svn.coderepos.org/share/lang/perl/HTTP-MobileAttribute/trunk@8857 d0d07461-0603-4401-acd4-de1884942a52

--- lib/HTTP/MobileAttribute.pm
+++ lib/HTTP/MobileAttribute.pm
@@ -1,7 +1,7 @@
 package HTTP::MobileAttribute;
 use strict;
 use warnings;
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 use Class::Component;
 use HTTP::MobileAttribute::Request;
 use HTTP::MobileAttribute::CarrierDetector;
