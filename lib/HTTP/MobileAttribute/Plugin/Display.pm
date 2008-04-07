f090792615b8a9aac9d6c19587c8dc7e46ce5c21
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-04-07 02:20:41 +0000

    added docs.
    git-svn-id: http://svn.coderepos.org/share/lang/perl/HTTP-MobileAttribute/trunk@9046 d0d07461-0603-4401-acd4-de1884942a52

--- lib/HTTP/MobileAttribute/Plugin/Display.pm
+++ lib/HTTP/MobileAttribute/Plugin/Display.pm
@@ -49,3 +49,41 @@ use base qw/Class::Accessor::Fast/;
 __PACKAGE__->mk_accessors(qw/width height color depth/);
 
 1;
+__END__
+
+=encoding UTF-8
+
+=head1 NAME
+
+HTTP::MobileAttribute::Plugin::Display - ディスプレイサイズの情報を得る
+
+=head1 DESCRIPTION
+
+    use HTTP::MobileAttribute plugins => [ {
+        module => 'Display',
+        config => {
+            DoCoMoMap => +{
+                D209I => +{ color => 1 depth => 256 height => 90 width => 96 },
+                # snip ...
+            }
+        }
+    }];
+
+    HTTP::MobileAttribute->new($ua)->display; # => instance of HTTP::MobileAttribute::Plugin::Display::Display
+
+=head1 DESCRIPTION
+
+'display' メソッドを呼ぶと、HTTP::MobileAttribute::Plugin::Display::Display のインスタンスをえることができます。
+このオブジェクトは width, height, color, depth の 4 種類の属性をもっています。
+
+DoCoMo は、HTTP Header 等からこの情報をうけとることができないので、データをプラグインにわたす必要があります。
+この情報は、DoCoMo からマシンリーダブルな形式では提供されていないので L<WWW::MobileCarrierJP> 等を駆使してスクレイピングする必要があります。
+
+=head1 AUTHORS
+
+Tokuhiro Matsuno
+
+=head1 SEE ALSO
+
+L<HTTP::MobileAttribute>
+
