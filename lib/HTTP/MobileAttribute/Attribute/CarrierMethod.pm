bf4ca6cdfcdadb6ad90f74ee27d13996f29fc937
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-04-07 01:46:17 +0000

    added docs.
    git-svn-id: http://svn.coderepos.org/share/lang/perl/HTTP-MobileAttribute/trunk@9043 d0d07461-0603-4401-acd4-de1884942a52

--- lib/HTTP/MobileAttribute/Attribute/CarrierMethod.pm
+++ lib/HTTP/MobileAttribute/Attribute/CarrierMethod.pm
@@ -19,9 +19,30 @@ sub register {
 
 __END__
 
+=encoding utf-8
+
+=head1 NAME
+
+HTTP::MobileAttribute::Attribute::CarrierMethod - キャリヤ専用メソッド定義用アトリビュート
+
 =head1 SYNOPSIS
 
-    sub foo : MobileMethod('DoCoMo') {
-        # your codes here
-    }
+    sub foo :CarrierMethod('DoCoMo') { }
+    sub bar :CarrierMethod('EZweb', 'method_name') { }
+
+=head1 DESCRIPTION
+
+HTTP::MobileAttribute::Plugin::* において、キャリヤごとのメソッドを定義するためにつかいます。
+
+第1引数は、キャリヤ名です。
+
+第2引数はメソッド名です。これは省略可能であり、省略した場合には、実メソッド名でコンテキストクラスにメソッド定義されます。
+
+=head1 AUTHORS
+
+Tokuhiro Matsuno
+
+=head1 SEE ALSO
+
+L<HTTP::MobileAttribute>
 
