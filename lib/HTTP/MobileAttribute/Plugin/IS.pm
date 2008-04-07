6c273e1f19e545f26a242c9dc1723ff645819338
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-04-07 02:25:13 +0000

    added docs.
    git-svn-id: http://svn.coderepos.org/share/lang/perl/HTTP-MobileAttribute/trunk@9047 d0d07461-0603-4401-acd4-de1884942a52

--- lib/HTTP/MobileAttribute/Plugin/IS.pm
+++ lib/HTTP/MobileAttribute/Plugin/IS.pm
@@ -39,3 +39,45 @@ sub is_non_mobile: Method {
 }
 
 1;
+__END__
+
+=encoding UTF-8
+
+=for stopwords DoCoMo SoftBank EZweb AirHPhone
+
+=head1 NAME
+
+HTTP::MobileAttribute::Plugin::IS - is_* を定義する
+
+=head1 METHODS
+
+=over 4
+
+=item is_docomo
+
+DoCoMo 端末かどうかを判定します。
+
+=item is_j_phone
+=item is_vodafone
+=item is_softbank
+
+SoftBank 端末かどうかを判定します。
+
+=item is_ezweb
+
+EZweb 端末かどうかを判定します。
+
+=item is_airh_phone
+
+AirHPhone かどうかを判定します。
+
+=item is_non_mobile
+
+モバイル端末ではないかどうかを判定します。
+
+=back
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno
+
