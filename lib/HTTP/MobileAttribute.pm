237fb0ccc0b60b88c0f61a3ecf35d581e6bd247e
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-04-07 03:04:27 +0000

    lang/perl/HTTP-MobileAttribute: Checking in changes prior to tagging of version 0.06.  Changelog diff is:
    === Changes
    ==================================================================
    --- Changes	(revision 10478)
    +++ Changes	(local)
    @@ -1,5 +1,9 @@
     Revision history for Perl extension HTTP::MobileAttribute
     
    +0.06
    +
    +    No changes.
    +
     0.05 It's beta.
     
         - added Plugin::IS::DoCoMo, Plugin::IS::EZweb(by clouder++)
    Unknown target: CHANGES.
    
    
    git-svn-id: http://svn.coderepos.org/share/lang/perl/HTTP-MobileAttribute/trunk@9061 d0d07461-0603-4401-acd4-de1884942a52

--- lib/HTTP/MobileAttribute.pm
+++ lib/HTTP/MobileAttribute.pm
@@ -1,7 +1,7 @@
 package HTTP::MobileAttribute;
 use strict;
 use warnings;
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 use HTTP::MobileAttribute::Request;
 use HTTP::MobileAttribute::CarrierDetector;
 use UNIVERSAL::require;
