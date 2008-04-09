9543c9f0db070044b0f0781580997acdbd1e67e1
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-04-09 08:16:53 +0000

    lang/perl/HTTP-MobileAttribute: Checking in changes prior to tagging of version 0.07.  Changelog diff is:
    === Changes
    ==================================================================
    --- Changes	(revision 11671)
    +++ Changes	(local)
    @@ -1,5 +1,10 @@
     Revision history for Perl extension HTTP::MobileAttribute
     
    +0.07
    +
    +    - added Plugin::Encoding.
    +    - added plugin dependency auto resolver(thanks to yappo++).
    +
     0.06
     
         No changes.
    
    
    git-svn-id: http://svn.coderepos.org/share/lang/perl/HTTP-MobileAttribute/trunk@9194 d0d07461-0603-4401-acd4-de1884942a52

--- lib/HTTP/MobileAttribute.pm
+++ lib/HTTP/MobileAttribute.pm
@@ -1,7 +1,7 @@
 package HTTP::MobileAttribute;
 use strict;
 use warnings;
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 use HTTP::MobileAttribute::Request;
 use HTTP::MobileAttribute::CarrierDetector;
 use UNIVERSAL::require;
