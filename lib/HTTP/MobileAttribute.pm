fea0537db29cc87bcaed11e79b9604d4f9daa233
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-04-11 06:04:02 +0000

    lang/perl/HTTP-MobileAttribute: Checking in changes prior to tagging of version 0.09.  Changelog diff is:
    === Changes
    ==================================================================
    --- Changes	(revision 11784)
    +++ Changes	(local)
    @@ -1,5 +1,10 @@
     Revision history for Perl extension HTTP::MobileAttribute
     
    +0.09
    +
    +    - renamed plugin: Plugin::ID => Plugin::UserID.
    +      for compatibility with HTTP::MobileUserID.
    +
     0.08
     
         - added Plugin::CIDR(by yappo++)
    
    
    git-svn-id: http://svn.coderepos.org/share/lang/perl/HTTP-MobileAttribute/trunk@9306 d0d07461-0603-4401-acd4-de1884942a52

--- lib/HTTP/MobileAttribute.pm
+++ lib/HTTP/MobileAttribute.pm
@@ -1,7 +1,7 @@
 package HTTP::MobileAttribute;
 use strict;
 use warnings;
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 use HTTP::MobileAttribute::Request;
 use HTTP::MobileAttribute::CarrierDetector;
 use UNIVERSAL::require;
