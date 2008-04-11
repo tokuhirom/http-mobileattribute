2b0053b5021110024ce1ffc07b049ba5290e5983
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-04-11 06:33:31 +0000

    lang/perl/HTTP-MobileAttribute: Checking in changes prior to tagging of version 0.11.1.  Changelog diff is:
    === Changes
    ==================================================================
    --- Changes	(revision 11794)
    +++ Changes	(local)
    @@ -1,5 +1,10 @@
     Revision history for Perl extension HTTP::MobileAttribute
     
    +0.11.1
    +
    +    - supportes_user_id => supports_user_id
    +      fixed typo...
    +
     0.11
     
         - Plugin::UserID->is_supported_user_id => supports_user_id
    
    
    git-svn-id: http://svn.coderepos.org/share/lang/perl/HTTP-MobileAttribute/trunk@9316 d0d07461-0603-4401-acd4-de1884942a52

--- lib/HTTP/MobileAttribute.pm
+++ lib/HTTP/MobileAttribute.pm
@@ -1,7 +1,7 @@
 package HTTP::MobileAttribute;
 use strict;
 use warnings;
-our $VERSION = '0.11';
+our $VERSION = '0.11.1';
 use HTTP::MobileAttribute::Request;
 use HTTP::MobileAttribute::CarrierDetector;
 use UNIVERSAL::require;
