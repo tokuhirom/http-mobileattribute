cb18ce9574902fe8499270244bfc7f652dc47850
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-03-25 23:46:12 +0900

    Checking in changes prior to tagging of version 0.14.  Changelog diff is:
    diff --git a/Changes b/Changes
    index 6c38c1b..0c0c61c 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,10 @@
     Revision history for Perl extension HTTP::MobileAttribute
    
    +0.14
    +
    +    - N-02A みたいな端末のときに端末の横幅がとれない問題に対応
    +    - added tools/make-docomo-display-map.pl
    +
     0.13
    
         - support HTTP::Headers::Fast

--- lib/HTTP/MobileAttribute.pm
+++ lib/HTTP/MobileAttribute.pm
@@ -1,7 +1,7 @@
 package HTTP::MobileAttribute;
 use strict;
 use warnings;
-our $VERSION = '0.13';
+our $VERSION = '0.14';
 use HTTP::MobileAttribute::Request;
 use HTTP::MobileAttribute::CarrierDetector;
 use UNIVERSAL::require;
