25fa4796ba87485c3900373da2279a5cf62691c4
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-06-04 23:09:47 +0900

    Checking in changes prior to tagging of version 0.16.  Changelog diff is:
    diff --git a/Changes b/Changes
    index df83e08..6ac0449 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Perl extension HTTP::MobileAttribute
    
    +0.16
    +
    +    - added Plugin::DoCoMo::Browser for detect i-mode broser version
    +
     0.15
    
         - i miss'ed packaging.

--- lib/HTTP/MobileAttribute.pm
+++ lib/HTTP/MobileAttribute.pm
@@ -1,7 +1,7 @@
 package HTTP::MobileAttribute;
 use strict;
 use warnings;
-our $VERSION = '0.15';
+our $VERSION = '0.16';
 use HTTP::MobileAttribute::Request;
 use HTTP::MobileAttribute::CarrierDetector;
 use UNIVERSAL::require;
