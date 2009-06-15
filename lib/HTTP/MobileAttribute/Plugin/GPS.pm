b34ecab3245cb5d74a6c5af65df496f93e5cd0ee
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-06-15 16:58:20 +0900

    BUG: use $c->req instead of %ENV. reported by nekokak++ & test case by nekokak++

--- lib/HTTP/MobileAttribute/Plugin/GPS.pm
+++ lib/HTTP/MobileAttribute/Plugin/GPS.pm
@@ -3,6 +3,8 @@ use strict;
 use warnings;
 use base qw/HTTP::MobileAttribute::Plugin/;
 
+__PACKAGE__->depends([qw/IS::ThirdForce/]);
+
 our $DoCoMoGPSModels = { map { $_ => 1 } qw(F661i F505iGPS) };
 
 # only for backward compatiblity
@@ -22,7 +24,8 @@ sub gc_i :CarrierMethod('DoCoMo', 'gps_compliant') {
 }
 
 sub gc_e :CarrierMethod('EZweb', 'gps_compliant') {
-    my @specs = split //, $ENV{ HTTP_X_UP_DEVCAP_MULTIMEDIA } || '';
+    my ($self, $c) = @_;
+    my @specs = split //, $c->request->get('x-up-devcap-multimedia');
     return defined $specs[ 1 ] && $specs[ 1 ] =~ /^[23]$/;
 }
 
