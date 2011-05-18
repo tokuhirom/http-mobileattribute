876249cc2ad175d42e5b249852ced2ce8211aaf1
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-05-18 13:19:54 +0900

    Checking in changes prior to tagging of version 0.23.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 62797ee..75056a0 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Perl extension HTTP::MobileAttribute
    
    +0.23
    +
    +    - Use of qw(...) as parentheses is deprecated
    +
     0.22
    
         - deprecated.

--- lib/HTTP/MobileAttribute.pm
+++ lib/HTTP/MobileAttribute.pm
@@ -1,7 +1,7 @@
 package HTTP::MobileAttribute;
 use strict;
 use warnings;
-our $VERSION = '0.22';
+our $VERSION = '0.23';
 use 5.008001;
 use HTTP::MobileAttribute::Request;
 use HTTP::MobileAttribute::CarrierDetector;
